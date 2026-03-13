41f9d007e84a3f80ad631c1d37185486efce16ef
AlejandroHP17 <alejandro.hernandezp@elektra.com.mx>
2026-03-13 14:32:12 -0600

    ahora si compila android

--- composeApp/src/commonMain/kotlin/litechnology/com/mx/planalimenticiokmp/data/local/database/AssetLoader.kt
+++ composeApp/src/commonMain/kotlin/litechnology/com/mx/planalimenticiokmp/data/local/database/AssetLoader.kt
@@ -3,6 +3,6 @@ package litechnology.com.mx.planalimenticiokmp.data.local.database
 /**
  * Interfaz para cargar archivos de assets de forma multiplataforma.
  */
-expect class AssetLoader() {
+expect class AssetLoader {
     suspend fun loadJsonFile(fileName: String): String?
 }
